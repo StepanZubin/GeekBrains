cbbdb5a606f65b4993c0d66d81639e9e813eac55
STEPAN <chichetka27@gmail.com>
2022-08-15 06:44:15 +0400

    add folders

---
+++ C_Sharp_introductory_course/FinalVerificationWork/Program.cs
@@ -0,0 +1,47 @@
+﻿// array of strings
+
+string[] arrayInput = new string[4] {"hello", "2", "world", ":-)"};
+int size = 0;
+
+int indexInput = 0;
+int indexOutput = 0; 
+   
+while (indexInput < arrayInput.Length)
+{
+    if (arrayInput[indexInput].Length <= 3)
+    {
+        size++;
+    }
+    indexInput++;
+}
+
+string[] arrayOutput = new string[size];
+indexInput = 0;
+while (indexInput < arrayInput.Length)
+{
+    if (arrayInput[indexInput].Length <= 3)
+    {
+        arrayOutput[indexOutput] = arrayInput[indexInput];
+        indexOutput++;
+    }
+    indexInput++;
+}
+
+PrintStringArray(arrayInput);
+Console.Write(" -> ");
+PrintStringArray(arrayOutput);
+
+void PrintStringArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
