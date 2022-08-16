ce9e9c4497858649e7aa97d3ecff5dfac6794c21
STEPAN <chichetka27@gmail.com>
2022-08-16 19:32:39 +0400

    add correct in folder 5_BubbleSorting

--- Bootcamp_1/5_BubbleSorting/Program.cs
+++ Bootcamp_1/5_BubbleSorting/Program.cs
@@ -7,3 +7,28 @@
 5. [0, 1, 3, 5, 7, 8, 9] 
 */
 
+Console.Write("Enter the number of array elements n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Enter array[{i}]: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Initial array: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array.Length - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
