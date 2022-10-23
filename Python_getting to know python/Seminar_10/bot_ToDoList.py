10317e1c7e604dca0306910ec8670675273c2fbb
STEPAN <chichetka27@gmail.com>
2022-10-24 00:30:45 +0400

    add P_S10 ToDoList HW

--- Python_getting to know python/Seminar_10/bot_ToDoList.py
+++ Python_getting to know python/Seminar_10/bot_ToDoList.py
@@ -1,8 +1,11 @@
+import linecache as lin
+import csv
 import operation as op
 import config as c
 import logging as l
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,  ConversationHandler
+from datetime import datetime as dt
 
 # Включим ведение журнала
 l.basicConfig(
@@ -10,13 +13,14 @@ l.basicConfig(
 )
 logger = l.getLogger(__name__)
 
-
-MAIN_MENU, WORKING_IN_MENU = range(2)
+TIME_NOW = dt.now().strftime('%D_%H:%M')
+MAIN_MENU, WORKING_IN_MENU, TASK_SEARCH, ADD, ADD_TIME_TASK, DELETE_TASK = range(6)
 
 
 def start(update, _):
     op.for_start_menu(update, _)
     return MAIN_MENU
+
 
 def main_menu(update, _):
     user_choice = update.message.text
@@ -26,20 +30,111 @@ def main_menu(update, _):
         return cancel(update, _)
     return WORKING_IN_MENU
 
+
 def working_in_menu(update, _):
     user_choice = update.message.text
+    
     if user_choice == 'СПИСОК':
         op.output_list(update, _) 
-    # elif user_choice == "НАЙТИ":  
-    # elif user_choice == "ДОБАВИТЬ": 
-    # elif user_choice == "УДАЛИТЬ":     
+    elif user_choice == "НАЙТИ":
+        op.button_output(update, "введите запрос: ")
+        return TASK_SEARCH  
+    elif user_choice == "ДОБАВИТЬ": 
+        op.button_output(update, "введите название задачи(дела): ")
+        return ADD
+
+    elif user_choice == "МЕНЮ":  
+        op.for_main_menu(update, _) 
+        return WORKING_IN_MENU
+    elif user_choice == "УДАЛИТЬ":  # недоделано !!!
+        update.message.reply_text("ЗАГЛУШКА !!!")
+        op.for_main_menu(update, _) 
+        return WORKING_IN_MENU
+
     elif user_choice == "ВЫЙТИ":
         return cancel(update, _)
     return op.for_main_menu(update, _)
+  
+
+def add_task(update, context):
+    '''
+    Считывание названия добавляемой задачи
+    '''
+    user_task_name = update.message.text
+    if user_task_name != 'СПИСОК' and user_task_name != 'НАЙТИ' and user_task_name != 'ДОБАВИТЬ' and user_task_name != 'УДАЛИТЬ' and user_task_name != 'ВЫЙТИ':
+        if len(user_task_name) > 2:
+            context.user_data['name'] = user_task_name
+            op.button_output(update, "введите время для задачи(дела) в формате --:-- ")
+            return ADD_TIME_TASK
+        else:
+            update.message.reply_text('Не менее 3 символов')
+            return
 
 
+def add_time_task(update, context):
+    '''
+    Считывание времени добавляемой задачи.
+    Запись в файл Названия и Времени новой задачи.
+    '''
+    list_task = []
+    user_time_task = update.message.text 
+    if user_time_task != 'СПИСОК' and user_time_task != 'НАЙТИ' and user_time_task != 'ДОБАВИТЬ' and user_time_task != 'УДАЛИТЬ' and user_time_task != 'ВЫЙТИ':
+        if len(user_time_task) == 5 and user_time_task[2] == ':':
+            temp = user_time_task.replace(':', '')
+            if temp.isdigit():
+                user_task_name = context.user_data.get('name')
+                list_task.append(user_task_name.capitalize())
+                list_task.append(user_time_task)
+                op.write_csv(list_task)
+                update.message.reply_text('Задача добавлена')
+                op.for_main_menu(update, context)
+            else:
+                update.message.reply_text("Введите время в формате --:-- ")
+                return 
+        else:
+            update.message.reply_text("Введите время в формате --:-- ")
+            return 
+    return WORKING_IN_MENU
 
 
+def task_search(update, _):
+    '''
+    Поиск и вывод задачи из списка дел
+    '''
+    user_str = update.message.text
+
+    line = []
+    count = 0
+    index = 0
+    if user_str != 'СПИСОК' and user_str != 'НАЙТИ' and user_str != 'ДОБАВИТЬ' and user_str != 'УДАЛИТЬ' and user_str != 'ВЫЙТИ' and user_str != 'МЕНЮ':
+        if len(user_str) == 0:
+            update.message.reply_text('вы ввели пустую строку!')
+            return task_search()
+        elif user_str.isalpha(): 
+            context = user_str.title() 
+            try:
+                open('S10_P_ToDo.csv')
+                with open('S10_P_ToDo.csv', encoding='utf-8') as csvfile:
+                    file_reader = list(csv.reader(csvfile, delimiter=','))
+                    for row in file_reader:
+                        if context.capitalize() in row:
+                            count += 1
+                            line.append(row) 
+                            index = file_reader.index(row) 
+                    if count == 0: 
+                        update.message.reply_text("задача не найдена в списке дел")
+                        return op.for_main_menu(update, _)
+                    else: 
+                        update.message.reply_text(lin.getline('S10_P_ToDo.csv', index + 1))
+                        # op.for_delete_menu(update, context) 
+                        return op.for_delete_menu(update, context)
+            except FileNotFoundError:
+                update.message.reply_text("список дел пуст: добавьте запись")
+        else: 
+            update.message.reply_text('некорректный ввод!')
+            return task_search()
+    op.for_main_menu(update, _)
+    return WORKING_IN_MENU
 
 
 def cancel(update, _):
@@ -60,8 +155,11 @@ if __name__ == '__main__':
         entry_points=[CommandHandler('start', start), ('cancel', cancel)],
         states={
             MAIN_MENU: [MessageHandler(Filters.regex('^(НАЧАТЬ|ВЫЙТИ)$'), main_menu)],
-            WORKING_IN_MENU: [MessageHandler(Filters.regex('^(СПИСОК|НАЙТИ|ДОБАВИТЬ|УДАЛИТЬ|ВЫЙТИ)$'), working_in_menu)],
-
+            WORKING_IN_MENU: [MessageHandler(Filters.regex('^(СПИСОК|НАЙТИ|ДОБАВИТЬ|ВЫЙТИ)$'), working_in_menu)],
+            TASK_SEARCH: [MessageHandler(Filters.text, task_search)],
+            ADD: [MessageHandler(Filters.text, add_task)],
+            ADD_TIME_TASK: [MessageHandler(Filters.text, add_time_task)],
+            # DELETE_TASK: [MessageHandler(Filters.text, delete_task)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
--- Python_getting to know python/Seminar_10/operation.py
+++ Python_getting to know python/Seminar_10/operation.py
@@ -1,5 +1,14 @@
 from telegram import ReplyKeyboardMarkup
+import linecache as lin
 import csv
+from typing import List
+
+
+def button_output(update, string):
+    reply_keyboard = [['СПИСОК', 'НАЙТИ', 'ДОБАВИТЬ', 'УДАЛИТЬ', 'ВЫЙТИ']] 
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+    update.message.reply_text(string, reply_markup=markup_key)
+
 
 def for_start_menu(update, _): 
     '''
@@ -15,11 +24,19 @@ def for_main_menu(update, _):
     '''
     Вывод кнопок ГЛАВного меню пользователю в телеграмм
     '''
-    reply_keyboard = [['СПИСОК', 'НАЙТИ', 'ДОБАВИТЬ', 'УДАЛИТЬ', 'ВЫЙТИ']] 
+    reply_keyboard = [['СПИСОК', 'НАЙТИ', 'ДОБАВИТЬ', 'ВЫЙТИ']] 
     markup_key = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
     update.message.reply_text("выберите действие"
-                            "\nСПИСОК, НАЙТИ, ДОБАВИТЬ, УДАЛИТЬ или ВЫЙТИ", reply_markup=markup_key)
+                            "\nСПИСОК, НАЙТИ, ДОБАВИТЬ или ВЫЙТИ", reply_markup=markup_key)
 
+def for_delete_menu(update, _):
+    '''
+    Вывод кнопок ПОДменю поиск записи
+    '''
+    reply_keyboard = [['УДАЛИТЬ', 'МЕНЮ']] 
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+    update.message.reply_text("выберите действие"
+                            "\nУДАЛИТЬ ЗАПИСЬ или МЕНЮ", reply_markup=markup_key)
 
 def output_list(update, _):  
     '''
@@ -29,27 +46,25 @@ def output_list(update, _):
         update.message.reply_text(csvfile.read())  
 
 
+def read_csv():
+    '''
+    Чтение из файла csv с возвращаемым списком
+    '''
+    with open('S10_P_ToDo.csv','r', encoding='utf-8') as f:
+        list_task = [{key: value  for key, value in task.items()}
+                for task in csv.DictReader(f, skipinitialspace=True)]
+    print(list_task)
+    return list_task
 
-#   НЕ СДЕЛАНО!!! 
-def contact_search(update, context):
+
+def write_csv(list_task: List):
     '''
-    Поиск записи в файле списка дел
+    Запись новой задачи в файл csv.
     '''
-    line = []
-    count = 0
-    try:
-        open('S10_P_ToDo.csv')
-        with open('S10_P_ToDo.csv', encoding='utf-8') as csvfile:
-            file_reader = list(csv.reader(csvfile, delimiter=','))
-            for row in file_reader:
-                if context.capitalize() in row:
-                    count += 1
-                    line.append(row)  # добавление строки в список 
-                    index = file_reader.index(row)  # индекс строки с искомым контактом
-                    #print(row)
-            if count == 0:
-                update.message.reply_text("задача не найдена в списке дел")
-                return None
-    except FileNotFoundError:
-        update.message.reply_text("список дел пуст: добавьте запись")
-    return index
+    with open('S10_P_ToDo.csv', 'a', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter= ',')
+        writer.writerow(list_task)
+
+
+   
+    
